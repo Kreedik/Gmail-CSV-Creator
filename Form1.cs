c2b309bdb28b77c037d9e80418258a181ba62444
Kreedik <d.tsiplin@outlook.com>
2022-05-13 12:40:38 +0300

    Dpre Release

--- Form1.cs
+++ Form1.cs
@@ -273,7 +273,7 @@ namespace WinFormsApp1
 
 
         }
-
+        
         private void проПрограмуToolStripMenuItem_Click(object sender, EventArgs e)
         {
             MessageBox.Show("Програма для швидкого створення CSV-файлів GOOGLE Mailbox Mass Creator", "Про програму Gmail CSV creator", MessageBoxButtons.OK, MessageBoxIcon.Information);
